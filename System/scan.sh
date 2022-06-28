5a973a1a18e30f97e9d7ef5cf8c6ce4a9807a4b0
i拥玥 <1678336204@qq.com>
2022-06-28 16:53:09 +0800

    update readme document

--- System/scan.sh
+++ System/scan.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Author：Alan Huang
-# Description:  使用GScan插件扫描主机，生成安全扫描报告并发送到指定邮箱
+#Author：Alan Huang
+#Description:  使用GScan插件扫描主机，生成安全扫描报告并发送到指定邮箱
+#搭配GScan使用
 
 mail='cmrhyq@163.com'
 src='/opt/GScan'
